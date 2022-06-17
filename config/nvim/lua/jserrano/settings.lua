d60129706a20389b5f0ad0332e256fb2f9a0fb32
Juan Diego Serrano <57143089+juandserrano@users.noreply.github.com>
2022-06-17 14:55:11 -0400

    Update settings.lua

--- config/nvim/lua/jserrano/settings.lua
+++ config/nvim/lua/jserrano/settings.lua
@@ -78,41 +78,6 @@ require('lualine').setup {
  extensions = {}
 }
 
--- luasnip setup
-local luasnip = require 'luasnip'
-
--- nvim-cmp setup
-local cmp = require 'cmp'
-cmp.setup {
-  snippet = {
-    expand = function(args)
-      require('luasnip').lsp_expand(args.body)
-    end,
-  },
-  mapping = {
-    ['<C-p>'] = cmp.mapping.select_prev_item(),
-    ['<C-n>'] = cmp.mapping.select_next_item(),
-    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<C-e>'] = cmp.mapping.close(),
-    ['<CR>'] = cmp.mapping.confirm {
-      behavior = cmp.ConfirmBehavior.Replace,
-      select = true,
-    },
-  },
-  sources = {
-    { name = 'nvim_lsp' },
-    { name = 'path' },
-    { name = 'luasnip' },
-    { name = 'buffer', keyword_length = 5 },
-  },
-  experimental = {
-    native_menu = false,
-    ghost_text = true,
-  }
-}
-
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all"
   ensure_installed = { "lua", "go", "bash", "typescript", "css"},
