764c186398df44f2f9da5e19ca69a427b68a9226
Juan Diego Serrano <57143089+juandserrano@users.noreply.github.com>
2022-06-17 14:53:38 -0400

    Update lsp.lua

--- config/nvim/lua/jserrano/lsp.lua
+++ config/nvim/lua/jserrano/lsp.lua
@@ -1,22 +1,57 @@
 local lspconfig = require'lspconfig'
-local completion = require'completion'
-local function custom_on_attach(client)
-  print('Attaching to ' .. client.name)
-  completion.on_attach(client)
+
+-- Add additional capabilities supported by nvim-cmp
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+local servers = { 'clangd', 'rust_analyzer', 'pyright', 'tsserver', 'gopls'}
+
+-- setup language servers here
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    capabilities = capabilities,
+  }
 end
-local default_config = {
-  on_attach = custom_on_attach,
+
+-- luasnip setup
+local luasnip = require 'luasnip'
+
+-- nvim-cmp setup
+local cmp = require 'cmp'
+cmp.setup {
+  snippet = {
+    expand = function(args)
+      require('luasnip').lsp_expand(args.body)
+    end,
+  },
+  mapping = {
+    ['<C-p>'] = cmp.mapping.select_prev_item(),
+    ['<C-n>'] = cmp.mapping.select_next_item(),
+    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.close(),
+    ['<CR>'] = cmp.mapping.confirm {
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true,
+    },
+  },
+  sources = {
+    { name = 'nvim_lsp' },
+    { name = 'path' },
+    { name = 'luasnip' },
+    { name = 'buffer', keyword_length = 5 },
+  },
+  experimental = {
+    native_menu = false,
+    ghost_text = true,
+  }
 }
 
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
   vim.lsp.diagnostic.on_publish_diagnostics, {
     underline = true,
-    virtual_text = false,
+    virtual_text = true,
     signs = true,
     update_in_insert = true,
   }
 )
-
--- setup language servers here
-lspconfig.tsserver.setup(default_config)
-lspconfig.gopls.setup(default_config)
