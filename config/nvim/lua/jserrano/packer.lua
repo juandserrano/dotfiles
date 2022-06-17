d22406ad277ca0e2ac83cc7ec23a3c93f44a9923
Juan Diego Serrano <57143089+juandserrano@users.noreply.github.com>
2022-06-17 14:54:17 -0400

    Update packer.lua

--- config/nvim/lua/jserrano/packer.lua
+++ config/nvim/lua/jserrano/packer.lua
@@ -6,7 +6,6 @@ return require('packer').startup(function()
   use 'nvim-lualine/lualine.nvim'
 -- LSP
   use 'neovim/nvim-lspconfig'
-  use 'nvim-lua/completion-nvim'
   use 'hrsh7th/nvim-cmp'
   use 'hrsh7th/cmp-nvim-lsp'
   use 'hrsh7th/cmp-buffer'
