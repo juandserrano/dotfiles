0540e7b55d354f377ec4c774dc26fa59b9a1a23c
Juan Diego Serrano <juandserrano@gmail.com>
2023-08-26 11:28:55 -0400

    20230826

--- installer.sh
+++ installer.sh
@@ -1,4 +1,6 @@
-#not a script yet
+#!/bin/bash
+
+# not a working script yet
 
 yay -S brave-bin neovim lazygit variety scrot volumeicon picom alacritty \
   obsidian solaar github-cli tmux timeshift zathura zathura-pdf-poppler
@@ -10,3 +12,8 @@ echo 'export PATH="$HOME/.local/bin:$PATH"' >> ~/.bashrc
 source ~/.bashrc
 nvm install node
 LV_BRANCH='release-1.3/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.3/neovim-0.9/utils/installer/install.sh)
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+ln -s $HOME/dotfiles/tmux.conf $HOME/.tmux.conf
+ln -s $HOME/dotfiles/config/lvim/ $HOME/.config/lvim
+ln -s $HOME/dotfiles/config/awesome/ $HOME/.config/awesome
+ln -s $HOME/dotfiles/config/alacritty/ $HOME/.config/alacritty
