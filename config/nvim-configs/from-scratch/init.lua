3d4327bb0f59395b967ad1ba87186e030cf07304
Juan Diego Serrano <juandserrano@gmail.com>
2024-02-27 08:29:49 -0500

    Add from-scratch

---
+++ config/nvim-configs/from-scratch/init.lua
@@ -0,0 +1,41 @@
+vim.o.expandtab = true
+vim.o.tabstop = 2
+vim.o.softtabstop = 2
+vim.o.shiftwidth = 2
+vim.g.mapleader = " "
+vim.o.hlsearch = false
+vim.o.wrap = false
+vim.o.scrolloff = 8
+vim.o.colorcolumn = "80"
+vim.o.rnu = true
+vim.o.undofile = true
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.wo.signcolumn = "yes"
+vim.o.termguicolors = true
+vim.o.breakindent = true
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup("plugins")
+require("config/keymaps")
+
+local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
+vim.api.nvim_create_autocmd("TextYankPost", {
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+	group = highlight_group,
+	pattern = "*",
+})
