1255929fc92d1057d1113bff0916b7632729cdc1
Juan Diego Serrano <57143089+juandserrano@users.noreply.github.com>
2024-05-26 18:41:15 -0400

    add autopairs

--- config/nvim-configs/from-scratch/lua/plugins.lua
+++ config/nvim-configs/from-scratch/lua/plugins.lua
@@ -1,9 +1,18 @@
 
 return {
+  {
   "catppuccin/nvim",
   name = "catppuccin",
   priority = 1000,
   config = function()
     vim.cmd.colorscheme "catppuccin"
   end
+  },
+  {
+    'windwp/nvim-autopairs',
+    event = "InsertEnter",
+    config = true
+    -- use opts = {} for passing setup options
+    -- this is equalent to setup({}) function
+  }
 }
