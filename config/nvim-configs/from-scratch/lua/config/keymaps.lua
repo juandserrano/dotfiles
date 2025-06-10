f0237f12b7ec182f4da56a9432b2cd3260b4d231
Juan Diego Serrano <juandserrano@gmail.com>
2025-06-09 21:05:24 -0400

    NEOVIM: add comment keymap

--- config/nvim-configs/from-scratch/lua/config/keymaps.lua
+++ config/nvim-configs/from-scratch/lua/config/keymaps.lua
@@ -19,7 +19,7 @@ vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
 vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
 vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, {})
-vim.keymap.set({ "n"}, "<space>cr", vim.lsp.buf.rename, {})
+vim.keymap.set({ "n" }, "<space>cr", vim.lsp.buf.rename, {})
 
 -- None-LS
 vim.keymap.set("n", "<leader>lf", vim.lsp.buf.format, {})
